a50394fcdd105060af6e45756d58cbe244c8cca4
Sherriff Agboola <rylxes@gmail.com>
2019-12-12 15:47:37 +0100

    db conn

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,23 +1,23 @@
 CREATE TABLE random_city (
   id bigint NOT NULL,
-  name varchar(255) DEFAULT NULL,
+  name varchar(191) DEFAULT NULL,
   PRIMARY KEY (id)
 );
 
 CREATE TABLE app_role (
   id bigint NOT NULL ,
-  description varchar(255) DEFAULT NULL,
-  role_name varchar(255) DEFAULT NULL,
+  description varchar(191) DEFAULT NULL,
+  role_name varchar(191) DEFAULT NULL,
   PRIMARY KEY (id)
 );
 
 
 CREATE TABLE app_user (
   id bigint NOT NULL ,
-  first_name varchar(255) NOT NULL,
-  last_name varchar(255) NOT NULL,
-  password varchar(255) NOT NULL,
-  username varchar(255) NOT NULL,
+  first_name varchar(191) NOT NULL,
+  last_name varchar(191) NOT NULL,
+  password varchar(191) NOT NULL,
+  username varchar(191) NOT NULL,
   PRIMARY KEY (id)
 );
 
@@ -30,37 +30,37 @@ CREATE TABLE user_role (
 
 drop table if exists oauth_client_details;
 create table oauth_client_details (
-  client_id VARCHAR(256) PRIMARY KEY,
-  resource_ids VARCHAR(256),
-  client_secret VARCHAR(256),
-  scope VARCHAR(256),
-  authorized_grant_types VARCHAR(256),
-  web_server_redirect_uri VARCHAR(256),
-  authorities VARCHAR(256),
+  client_id VARCHAR(191) PRIMARY KEY,
+  resource_ids VARCHAR(191),
+  client_secret VARCHAR(191),
+  scope VARCHAR(191),
+  authorized_grant_types VARCHAR(191),
+  web_server_redirect_uri VARCHAR(191),
+  authorities VARCHAR(191),
   access_token_validity INTEGER,
   refresh_token_validity INTEGER,
-  additional_information VARCHAR(4096),
-  autoapprove VARCHAR(256)
+  additional_information VARCHAR(191),
+  autoapprove VARCHAR(191)
 );
 
 drop table if exists oauth_client_token;
 create table oauth_client_token (
-token_id VARCHAR(255),
+token_id VARCHAR(191),
 token blob ,
-authentication_id VARCHAR(255),
-user_name VARCHAR(255),
-client_id VARCHAR(255)
+authentication_id VARCHAR(191),
+user_name VARCHAR(191),
+client_id VARCHAR(191)
 );
 
 drop table if exists oauth_access_token;
 create table oauth_access_token (
-token_id VARCHAR(255),
+token_id VARCHAR(191),
 token blob,
-authentication_id VARCHAR(255),
-user_name VARCHAR(255),
-client_id VARCHAR(255),
+authentication_id VARCHAR(191),
+user_name VARCHAR(191),
+client_id VARCHAR(191),
 authentication blob,
-refresh_token VARCHAR(255)
+refresh_token VARCHAR(191)
 );
 
 create index oauth_access_token_id on oauth_access_token(token_id);
@@ -68,37 +68,37 @@ create index oauth_refresh_token_id on oauth_access_token(token_id);
 
 drop table if exists oauth_refresh_token;
 create table oauth_refresh_token (
-token_id VARCHAR(255),
+token_id VARCHAR(191),
 token blob,
 authentication blob
 );
 drop table if exists oauth_code;
 create table oauth_code (
-code VARCHAR(255), authentication blob
+code VARCHAR(191), authentication blob
 );
 
 drop table if exists oauth_approvals;
 create table oauth_approvals (
-userId VARCHAR(255),
-clientId VARCHAR(255),
-scope VARCHAR(255),
+userId VARCHAR(191),
+clientId VARCHAR(191),
+scope VARCHAR(191),
 status VARCHAR(10),
 expiresAt TIMESTAMP,
 lastModifiedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 drop table if exists ClientDetails;
 create table ClientDetails (
-appId VARCHAR(255) PRIMARY KEY,
-resourceIds VARCHAR(255),
-appSecret VARCHAR(255),
-scope VARCHAR(255),
-grantTypes VARCHAR(255),
-redirectUrl VARCHAR(255),
-authorities VARCHAR(255),
+appId VARCHAR(191) PRIMARY KEY,
+resourceIds VARCHAR(191),
+appSecret VARCHAR(191),
+scope VARCHAR(191),
+grantTypes VARCHAR(191),
+redirectUrl VARCHAR(191),
+authorities VARCHAR(191),
 access_token_validity INTEGER,
 refresh_token_validity INTEGER,
-additionalInformation VARCHAR(4096),
-autoApproveScopes VARCHAR(255)
+additionalInformation VARCHAR(191),
+autoApproveScopes VARCHAR(191)
 );
 
 INSERT INTO app_role (id, role_name, description) VALUES (1, 'STANDARD_USER', 'Standard User - Has no admin rights');
